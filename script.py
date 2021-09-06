a027d451a8beb2c2fca9fba9d4fa810f409c678a
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-06 11:39:11 +0530

    Update script.py

--- script.py
+++ script.py
@@ -52,9 +52,9 @@ See <i>/help</i> for commands and more details.</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : ᗪᗩᖇK ᒪOᖇᗪ</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @Beluga100000    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
