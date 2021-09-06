f6df250c1f1b7af741695882b974d036dfd15d5f
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-06 12:46:52 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ class Script(object):
     START_MSG = """<b>Hy {},
 Im a simple bot which is designed and built for adding filters in any group.
 
+<a href="https://t.me/Beluga100000">ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ</a>
+
 See <i>/help</i> for commands and more details.</b>
 """
 
