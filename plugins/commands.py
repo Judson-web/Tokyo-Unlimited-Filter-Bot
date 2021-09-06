63a79d9763541099dfbb724b8a825da3ef43a292
KING XV <85796566+Judson-web@users.noreply.github.com>
2021-09-06 15:41:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE VENO😇", url="https://github.com/Judson-web/Unlimited-Filter-Bot")
+                        "SOURCE CODE VENO 😇", url="https://github.com/Judson-web/Unlimited-Filter-Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
