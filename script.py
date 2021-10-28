ffd0eb99c0e38bfef2e753fce82d778ff8bd4445
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-10-28 12:56:52 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,75 +1,32 @@
 class Script(object):
 
-    START_MSG = """<b>𝙃𝙮 {},
-
-𝙄'𝙢 𝙖𝙣 𝙖𝙙𝙫𝙖𝙣𝙘𝙚𝙙 𝙛𝙞𝙡𝙩𝙚𝙧 𝙗𝙤𝙩 𝙬𝙞𝙩𝙝 𝙢𝙖𝙣𝙮 𝙘𝙖𝙥𝙖𝙗𝙞𝙡𝙞𝙩𝙞𝙚𝙨!
-𝙏𝙝𝙚𝙧𝙚 𝙞𝙨 𝙣𝙤 𝙥𝙧𝙖𝙘𝙩𝙞𝙘𝙖𝙡 𝙡𝙞𝙢𝙞𝙩𝙨 𝙛𝙤𝙧 𝙢𝙮 𝙛𝙞𝙡𝙩𝙚𝙧𝙞𝙣𝙜 𝙘𝙖𝙥𝙖𝙘𝙞𝙩𝙮 :)
-
-<b>/about - ᴹʸ ᴵⁿᶠᵒʳᵐᵃᵗⁱᵒⁿ 👩‍🎤</b>
-<b>/info - ʸᵒᵘʳ ᴵⁿᶠᵒʳᵐᵃᵗⁱᵒⁿ 🧠</b>
-
-𝙎𝙚𝙚 <i>/help</i> 𝙛𝙤𝙧 𝙘𝙤𝙢𝙢𝙖𝙣𝙙𝙨 𝙖𝙣𝙙 𝙢𝙤𝙧𝙚 𝙙𝙚𝙩𝙖𝙞𝙡𝙨.
-
-𝙊𝙬𝙣𝙚𝙧 😎 <a href="https://t.me/VAMPIRE_KING_NO_1">ᗪᗩᖇK ✞Oᖇᗪ</a>
-</b>
+    START_MSG = """<b>𝘏𝘦𝘭𝘭𝘰 {},
+𝘐𝘮 𝘢 𝘴𝘪𝘮𝘱𝘭𝘦 𝘣𝘰𝘵 𝘸𝘩𝘪𝘤𝘩 𝘪𝘴 𝘥𝘦𝘴𝘪𝘨𝘯𝘦𝘥 𝘢𝘯𝘥 𝘣𝘶𝘪𝘭𝘵 𝘧𝘰𝘳 𝘢𝘥𝘥𝘪𝘯𝘨 𝘧𝘪𝘭𝘵𝘦𝘳𝘴 𝘪𝘯 𝘢𝘯𝘺 𝘨𝘳𝘰𝘶𝘱.𝘏𝘪𝘵 /𝘩𝘦𝘭𝘱 𝘧𝘰𝘳 𝘮𝘰𝘳𝘦 𝘪𝘯𝘧𝘰𝘳𝘮𝘢𝘵𝘪𝘰𝘯.
 """
 
 
     HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
+<i>𝙒𝙝𝙖𝙩 𝙞𝙨 𝙖 𝙛𝙞𝙡𝙩𝙚𝙧 𝙗𝙤𝙩?</i>
 
+<b>A bot were group admins can set replies for a particular keyword and the bot will automatically send preset replies whenever that keyword enountered in the chat.</b>
 
-<b>Basic Commands;</b>
+<b>Usual commands:</b>
 
-/start - Check if I'm alive!
-/help - Command help
-/about - Something about me!
-
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
-/id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
-<b>© @Beluga100000</b>
+/start - ```Check if I'm alive!```
+/help - ```Command help```
+/about - ```Something about me!```
 """
 
 
-    ABOUT_MSG = """<b>🙋🏻‍♂️   Hellooo😇
+    ABOUT_MSG = """<b>🙋🏻‍♂️ 𝘏𝘦𝘭𝘭𝘰 {},
     
-⭕️<b>My Name : Tokyo</b>
+⭕️<b>My Name :</b> Filter Bot</b>
 
-⭕️<b>Creater :</b> @Beluga100000 
+⭕️<b>Creater :</b>  <a href="https://t.me/VAMPIRE_KING_NO_1">ƬЄƦƦƠƦ MƖƇƘЄƳ</a>
 
 ⭕️<b>Credits :</b> Everyone in this journey ☺️
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>🎥 sᴛᴏʀʏ ᴛɪᴍᴇ ᴄʜᴀɴɴᴇʟ🎥 : https://t.me/storytym
-
 """
